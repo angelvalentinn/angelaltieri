a92bdad162034c4c1b3718504e7212d59dbc0d3b
angelvalentinn <altieriangel1@gmail.com>
2023-06-07 03:38:22 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* TIPADO AUTOMÁTICO CON TYPED.JS */
+let typed = new Typed('.header__subtitle', {
+    strings: ['Estudiante de Sistemas','Desarrollador trainee'],
+    typeSpeed: 50,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    showCursor: false,
+});
