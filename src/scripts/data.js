b7e48d0553099f408160996578ec4d3852365b16
angelvalentinn <altieriangel1@gmail.com>
2024-03-02 01:39:39 -0300

    first commit

---
+++ src/scripts/data.js
@@ -0,0 +1,121 @@
+const DATA_PROJECTS = [
+    {
+        title: 'E-commerce',
+        description: 'Ecommerce copia UI de Mercado Libre. Proyecto final de CoderHouse.',
+        img: '../../../imgs_proyects/mercado-libre.webp',
+        tecnologias: [
+            'React',
+            'SASS',
+            'Firebase'
+        ],
+        repo: 'https://github.com/angelvalentinn/proyecto-final-coderhouse',
+        deploy: 'https://ecommerce-altieri.netlify.app/',
+    },
+
+    {
+        title: 'App games',
+        description: 'Proyecto acerca de datos de videojuegos, haciendo uso de una API.',
+        img: '../../../imgs_proyects/app-games.webp',
+        tecnologias: [
+            'React',
+            'SASS',
+            'RAWG API'
+        ],
+        repo: 'https://github.com/angelvalentinn/app-games',
+        deploy: 'https://game-app-react-altieri.netlify.app/',
+    },
+
+    {
+        title: 'Movie App',
+        description: 'Proyecto acerca de datos de videojuegos.',
+        img: '../../../imgs_proyects/movie-app.webp',
+        tecnologias: [
+            'React',
+            'Tailwind',
+            'TMB API'
+        ],
+        repo: 'https://github.com/angelvalentinn/movie-app-react',
+        deploy: 'https://movie-app-react-angel.netlify.app/',
+    },
+
+    {
+        title: 'Poke Game',
+        description: 'Proyecto acerca de un mini juego de adivinanza de pokemones.',
+        img: '../../../imgs_proyects/pokegame.webp',
+        tecnologias: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Poke Api'
+        ],
+        repo: 'https://github.com/angelvalentinn/Pokegame',
+        deploy: 'https://angelvalentinn.github.io/Pokegame/',
+    },
+
+    {
+        title: 'Calculadora',
+        description: 'Proyecto de una calculadora con JS nativo.',
+        img: '../../../imgs_proyects/calculadora.webp',
+        tecnologias: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        repo: 'https://github.com/angelvalentinn/Calculadora',
+        deploy: 'https://angelvalentinn.github.io/Calculadora/',
+    },
+
+    {
+        title: 'Memotest',
+        description: 'Proyecto del juego Memo test. Mi primer proyecto en React.',
+        img: '../../../imgs_proyects/memogame.webp',
+        tecnologias: [
+            'React',
+            'Tailwind',
+        ],
+        repo: 'https://github.com/angelvalentinn/memotest',
+        deploy: 'https://memotest-altieri.netlify.app/',
+    },
+
+    {
+        title: 'Piedra, papel o tijera.',
+        description: 'Proyecto sobre piedra, papel o tijera.',
+        img: '../../../imgs_proyects/ppot.webp',
+        tecnologias: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        repo: 'https://github.com/angelvalentinn/rock-paper-scissor',
+        deploy: 'https://angelvalentinn.github.io/rock-paper-scissor/',
+    },
+
+    {
+        title: 'App Clima',
+        description: 'Proyecto sobre buscador de Clima, consumiendo API.',
+        img: '../../../imgs_proyects/app-clima.webp',
+        tecnologias: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Weather API'
+        ],
+        repo: 'https://github.com/angelvalentinn/app-clima',
+        deploy: 'https://angelvalentinn.github.io/app-clima/',
+    },
+
+    {
+        title: 'To do List',
+        description: 'Proyecto CRUD persistente con Local Storage.',
+        img: '../../../imgs_proyects/todolist.webp',
+        tecnologias: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        repo: 'https://github.com/angelvalentinn/todolist',
+        deploy: 'https://angelvalentinn.github.io/todolist/',
+    },
+]
+
+export default DATA_PROJECTS;
